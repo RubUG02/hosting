56a9db455a14ea9d79a4b1cbebb41da0766344c0
RubUG02 <74696001+RubUG02@users.noreply.github.com>
2024-09-18 18:34:29 -0600

    Add files via upload
    Cambios con mensaje chido

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 onload = () =>{
+        
+        alert("Gracias por seguir aquí, gracias por seguirlo intentando, gracias por ser tú. TQM");
         document.body.classList.remove("container");
 };
